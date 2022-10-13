9284ebe84922484d8c33294af2c1908b26862ff5
Nariman <Nariman079i>
2022-10-13 18:00:33 +0300

    create models

--- main/models.py
+++ main/models.py
@@ -4,3 +4,15 @@ class Sity(models.Model):
     title = models.CharField(max_length=60)
     def __str__(self):
         return self.title
+
+class Users(models.Model):
+    first_name = models.CharField(max_length=50,null=False)
+    email = models.EmailField(max_length=40,null=False, primary_key=True)
+
+    def __str__(self):
+        return self.email
+
+class Industrial(models.Model):
+    title = models.CharField(max_length=40,primary_key=True)
+
+
